f4bf75d0a459cd9850ed1c421d709f23e8e67b99
ortus <rush.b.fast113@gmail.com>
2023-01-05 14:09:18 +0300

    Slider class

---
+++ Slider.py
@@ -0,0 +1,42 @@
+import pygame
+
+
+class Slider:
+    # x - координата прямоугольника
+    # y - координата прямоугольника
+    # h - высота прямоугольника
+    # w - ширина прямоугольника
+    # r - радиус круга
+    def __init__(self, x, y, h, w, r, circle_color, rect_color, screen):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.r = r
+        self.circle_color = circle_color
+        self.rect_color = rect_color
+        self.slider_x = self.x
+        self.screen = screen
+
+    def draw_slider(self):
+        pygame.draw.rect(self.screen, self.rect_color, (self.x, self.y,  self.w, self.h))
+        pygame.draw.circle(self.screen, self.circle_color, (self.slider_x, self.y + self.h // 2), self.r)
+
+    # возвращает значение в %
+    def value(self):
+        return int((self.slider_x - self.x) / self.w * 100)
+
+    # эту функцию нужно вызывать в постоянно, чтобы круг не выходил за границы
+    def check(self):
+        button = pygame.mouse.get_pressed()
+        if button[0]:
+            pos = pygame.mouse.get_pos()
+            x = pos[0]
+            if x > self.x + self.w:
+                self.slider_x = self.x + self.w
+            elif x < self.x:
+                self.slider_x = self.x
+            else:
+                self.slider_x = x
+
+
