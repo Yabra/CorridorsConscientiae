3b4e9f03c13633d376b55f9d5a2a956c88e218b0
YabraGameDev <YabraDeveloper@yandex.ru>
2023-07-10 13:37:32 +0500

    Debaff cheats added

--- cheats.py
+++ cheats.py
@@ -2,10 +2,16 @@ import pygame
 
 
 def check_cheat_key(game, event):
-    if event.mod & pygame.KMOD_CTRL and event.mod & pygame.KMOD_LSHIFT:
+    if event.mod & pygame.KMOD_CTRL:
         if event.key == pygame.K_n:
             game.next_level()
         elif event.key == pygame.K_m:
             game.heal_mind()
         elif event.key == pygame.K_l:
             game.remove_lostness()
+
+    if event.mod & pygame.KMOD_LALT:
+        if event.key == pygame.K_n:
+            game.previous_level()
+        elif event.key == pygame.K_l:
+            game.add_lostness()
